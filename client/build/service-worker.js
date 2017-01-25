0be0869a2185d3660b99594e8b38bc76fd431e62
Christiano Milfont <cmilfont@gmail.com>
2017-01-25 07:35:25 -0300

    [#master] Manifest ajustado

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","9cfe540dbdfbde48e6ba1579e3c22b6d"],["manifest.json","ab549f7ef2e3adbe06a4cb080dfd1f08"],["static/css/main.853c1ad2.css","c5ff60e4db79bb7b40b96127fbd932cd"],["static/js/main.7dcfa33c.js","54fabcb7030fe8007cab03aa938d9dde"]];
+var precacheConfig = [["index.html","9cfe540dbdfbde48e6ba1579e3c22b6d"],["manifest.json","7b1f59f28f847533132fc8c198a7861e"],["static/css/main.853c1ad2.css","c5ff60e4db79bb7b40b96127fbd932cd"],["static/js/main.7dcfa33c.js","54fabcb7030fe8007cab03aa938d9dde"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
