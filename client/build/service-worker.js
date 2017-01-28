bf24df9dc7fa9a7d1960ae2183bf46f4c8368f26
Christiano Milfont <cmilfont@gmail.com>
2017-01-28 17:58:39 -0300

    [#master] new version

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","32afaf70ffa3423369623cfb301caeab"],["manifest.json","0e0607a4618e86451d4e18c8a0770a3e"],["static/css/main.87ddb66a.css","1060182ab6c81952c451a17cce0ca3f0"],["static/js/main.74c60eb5.js","cc64d33c58ecfd7d143ee951a3f13747"]];
+var precacheConfig = [["index.html","9945b2d5299bbf12c7dcbc6670467760"],["manifest.json","0e0607a4618e86451d4e18c8a0770a3e"],["static/css/main.87ddb66a.css","1060182ab6c81952c451a17cce0ca3f0"],["static/js/main.93a44aa3.js","982008af982e671c447123573a3d086c"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
